62198eca82599a81a56961d9e667d5d393789c7f
Rakesh Mistry <mistryrn@mcmaster.ca>
2023-09-12 12:47:59 -0400

    ♻️  Refactor Data Mapper Services * Eliminate need for hoisting in Data Mapper `search` and `create` services * Add `TODO`s to track necessary work to transition from `fetch` to `axios` for HTTP requests in upcoming ticket #72 : https://github.com/OHCRN/platform/issues/72

--- apps/data-mapper/src/service/create.ts
+++ apps/data-mapper/src/service/create.ts
@@ -3,6 +3,84 @@
 
 import { getAppConfig } from '../config';
 
+// PI-DAS
+const createParticipantPiData = async ({
+	name,
+	email,
+}: {
+	name: string;
+	email: string;
+}): Promise<any> => {
+	// TODO: add Type instead of any
+	const { piDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${piDasUrl}/participants`, {
+		method: 'POST',
+		body: JSON.stringify({ name, email }),
+		headers: { 'Content-Type': 'application/json' },
+	}).then((res) => res.json());
+	return result.participant;
+};
+
+// KEYS-DAS
+// TODO: add Type instead of any
+const createParticipantOhipKey = async (participantId: string): Promise<any> => {
+	const { keysDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${keysDasUrl}/ohip-keys`, {
+		method: 'POST',
+		body: JSON.stringify({ participantId }),
+		headers: { 'Content-Type': 'application/json' },
+	}).then((res) => res.json());
+	return result.ohipKey;
+};
+
+// PHI-DAS
+const saveParticipantOhipNumber = async ({
+	ohipPrivateKey,
+	ohipNumber,
+}: {
+	ohipPrivateKey: string;
+	ohipNumber: string;
+}): Promise<any> => {
+	// TODO: add Type instead of any
+	const { phiDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${phiDasUrl}/ohip`, {
+		method: 'POST',
+		body: JSON.stringify({ ohipPrivateKey, ohipNumber }),
+		headers: { 'Content-Type': 'application/json' },
+	}).then((res) => res.json());
+	return result.ohipData.ohipNumber;
+};
+
+// CONSENT-DAS
+const createParticipantConsentData = async ({
+	participantId,
+	emailVerified,
+}: {
+	participantId: string;
+	emailVerified: boolean;
+}): Promise<any> => {
+	// TODO: add Type instead of any
+	const { consentDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${consentDasUrl}/participants`, {
+		method: 'POST',
+		body: JSON.stringify({ participantId, emailVerified }),
+		headers: { 'Content-Type': 'application/json' },
+	}).then((res) => res.json());
+	return result.participant;
+};
+
 // separates data along concerns to store in respective DASes
 export const createParticipant = async ({
 	name,
@@ -34,78 +112,4 @@ export const createParticipant = async ({
 		ohipNumber: participantOhipNumber,
 		emailVerified: participantConsentData.emailVerified,
 	};
-};
-
-// PI-DAS
-const createParticipantPiData = async ({
-	name,
-	email,
-}: {
-	name: string;
-	email: string;
-}): Promise<any> => {
-	// TODO: add Type instead of any
-	const { piDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${piDasUrl}/participants`, {
-		method: 'POST',
-		body: JSON.stringify({ name, email }),
-		headers: { 'Content-Type': 'application/json' },
-	}).then((res) => res.json());
-	return result.participant;
-};
-
-// KEYS-DAS
-// TODO: add Type instead of any
-const createParticipantOhipKey = async (participantId: string): Promise<any> => {
-	const { keysDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${keysDasUrl}/ohip-keys`, {
-		method: 'POST',
-		body: JSON.stringify({ participantId }),
-		headers: { 'Content-Type': 'application/json' },
-	}).then((res) => res.json());
-	return result.ohipKey;
-};
-
-// PHI-DAS
-const saveParticipantOhipNumber = async ({
-	ohipPrivateKey,
-	ohipNumber,
-}: {
-	ohipPrivateKey: string;
-	ohipNumber: string;
-}): Promise<any> => {
-	// TODO: add Type instead of any
-	const { phiDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${phiDasUrl}/ohip`, {
-		method: 'POST',
-		body: JSON.stringify({ ohipPrivateKey, ohipNumber }),
-		headers: { 'Content-Type': 'application/json' },
-	}).then((res) => res.json());
-	return result.ohipData.ohipNumber;
-};
-
-// CONSENT-DAS
-const createParticipantConsentData = async ({
-	participantId,
-	emailVerified,
-}: {
-	participantId: string;
-	emailVerified: boolean;
-}): Promise<any> => {
-	// TODO: add Type instead of any
-	const { consentDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${consentDasUrl}/participants`, {
-		method: 'POST',
-		body: JSON.stringify({ participantId, emailVerified }),
-		headers: { 'Content-Type': 'application/json' },
-	}).then((res) => res.json());
-	return result.participant;
 };
--- apps/data-mapper/src/service/search.ts
+++ apps/data-mapper/src/service/search.ts
@@ -3,6 +3,52 @@
 
 import { getAppConfig } from '../config';
 
+// PI-DAS
+// TODO: add Type instead of any
+const getParticipantPiData = async (participantId: string): Promise<any> => {
+	const { piDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${piDasUrl}/participants/${participantId}`).then((res) => res.json());
+	return result.participant;
+};
+
+// KEYS-DAS
+// TODO: add Type instead of any
+const getParticipantOhipKey = async (participantId: string): Promise<any> => {
+	const { keysDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${keysDasUrl}/ohip-keys/${participantId}`).then((res) => res.json());
+	return result.ohipKey.ohipPrivateKey;
+};
+
+// PHI-DAS
+// TODO: add Type instead of any
+const getParticipantOhipNumber = async (ohipPrivateKey: string): Promise<any> => {
+	const { phiDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${phiDasUrl}/ohip/${ohipPrivateKey}`).then((res) => res.json());
+	return result.ohipData.ohipNumber;
+};
+
+// CONSENT-DAS
+// TODO: add Type instead of any
+const getParticipantConsentData = async (participantId: string): Promise<any> => {
+	const { consentDasUrl } = getAppConfig();
+	// TODO: use urlJoin
+	// TODO: add error handling
+	// TODO: use axios instead of fetch
+	const result = await fetch(`${consentDasUrl}/participants/${participantId}`).then((res) =>
+		res.json(),
+	);
+	return result.participant;
+};
+
 // combines data from respective DASes to return a single Participant object
 // TODO: add Type instead of any
 export const getParticipant = async (participantId: string): Promise<any> => {
@@ -16,46 +62,4 @@ export const getParticipant = async (participantId: string): Promise<any> => {
 		ohipNumber: participantOhipNumber,
 		emailVerified: participantConsentData.emailVerified,
 	};
-};
-
-// PI-DAS
-// TODO: add Type instead of any
-const getParticipantPiData = async (participantId: string): Promise<any> => {
-	const { piDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${piDasUrl}/participants/${participantId}`).then((res) => res.json());
-	return result.participant;
-};
-
-// KEYS-DAS
-// TODO: add Type instead of any
-const getParticipantOhipKey = async (participantId: string): Promise<any> => {
-	const { keysDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${keysDasUrl}/ohip-keys/${participantId}`).then((res) => res.json());
-	return result.ohipKey.ohipPrivateKey;
-};
-
-// PHI-DAS
-// TODO: add Type instead of any
-const getParticipantOhipNumber = async (ohipPrivateKey: string): Promise<any> => {
-	const { phiDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${phiDasUrl}/ohip/${ohipPrivateKey}`).then((res) => res.json());
-	return result.ohipData.ohipNumber;
-};
-
-// CONSENT-DAS
-// TODO: add Type instead of any
-const getParticipantConsentData = async (participantId: string): Promise<any> => {
-	const { consentDasUrl } = getAppConfig();
-	// TODO: use urlJoin
-	// TODO: add error handling
-	const result = await fetch(`${consentDasUrl}/participants/${participantId}`).then((res) =>
-		res.json(),
-	);
-	return result.participant;
 };
