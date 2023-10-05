c375c2c1452a9453277f28196c46292170dc53c6
Ann Catton <anncatton@gmail.com>
2023-10-05 07:46:04 -0400

    rename common to types, make all packages esm modules

--- packages/types/src/entities/ConsentCategory.ts
+++ packages/types/src/entities/ConsentCategory.ts
@@ -1,13 +1,5 @@
 import * as z from 'zod';
 
-export const User = z.object({
-	id: z.string(),
-	name: z.string(),
-	email: z.string().email(),
-});
-
-export type User = z.infer<typeof User>;
-
 const CONSENT_CATEGORIES = [
 	'INFORMED_CONSENT',
 	'CONSENT_RELEASE_DATA',
