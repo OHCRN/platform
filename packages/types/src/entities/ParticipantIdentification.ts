100225b1079bed67afeb8be92ab0d47d2bcf9609
Alyssa D'Souza <70766739+alyssadsouza@users.noreply.github.com>
2023-10-11 16:55:39 -0400

    add province enum

--- packages/types/src/entities/ParticipantIdentification.ts
+++ packages/types/src/entities/ParticipantIdentification.ts
@@ -2,6 +2,25 @@ import * as z from 'zod';
 
 import { ConsentGroup } from './ConsentCategory.js';
 
+const PROVINCES = [
+	'ALBERTA',
+	'BRITISH_COLUMBIA',
+	'MANITOBA',
+	'NEW_BRUNSWICK',
+	'NEWFOUNDLAND_AND_LABRADOR',
+	'NORTHWEST_TERRITORIES',
+	'NOVA_SCOTIA',
+	'NUNAVUT',
+	'ONTARIO',
+	'PRINCE_EDWARD_ISLAND',
+	'QUEBEC',
+	'SASKATCHEWAN',
+	'YUKON',
+] as const;
+
+export const Province = z.enum(PROVINCES);
+export type Province = z.infer<typeof Province>;
+
 export const ParticipantIdentification = z.object({
 	id: z.string().cuid(),
 	inviteId: z.string().cuid().optional(),
@@ -13,14 +32,11 @@ export const ParticipantIdentification = z.object({
 		.string()
 		.regex(/^[A-Za-z\s]+$/)
 		.optional(),
-	dateOfBirth: z.date(), // TODO: check if needs to be string().datetime()
+	dateOfBirth: z.date(),
 	phoneNumber: z.string().regex(/[0-9]{10}/),
 	mailingAddressStreet: z.string().optional(),
 	mailingAddressCity: z.string().optional(),
-	mailingAddressProvince: z
-		.string()
-		.regex(/(AB|BC|MB|NB|NL|NS|NT|NU|ON|PE|QC|SK|YT)/) // TODO: confirm with spreadsheet values
-		.optional(),
+	mailingAddressProvince: Province.optional(),
 	mailingAddressPostalCode: z
 		.string()
 		.regex(/^[A-Za-z][0-9][A-Za-z][0-9][A-Za-z][0-9]/)
