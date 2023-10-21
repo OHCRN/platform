82daae0b0e71ef4bede2be47a674fc3dadbacafe
Ann Catton <anncatton@gmail.com>
2023-10-21 18:44:52 -0400

    export en dictionaries as dictionary types

--- apps/consent-ui/src/i18n/locales/en/ancestry.ts
+++ apps/consent-ui/src/i18n/locales/en/ancestry.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 const dictionary = {
 	ABORIGINAL_FROM_CENTRAL_SOUTH_AMERICA_EXCEPT_ARAWAK_AND_MAYA:
 		'Aboriginal from Central/South America (except Arawak and Maya)',
@@ -255,6 +274,9 @@ const dictionary = {
 	YUGOSLAVIAN_NOS: 'Yugoslavian, n.o.s.',
 	ZAMBIAN: 'Zambian',
 	ZIMBABWEAN: 'Zimbabwean',
-};
+} as const;
+
+export type AncestryDictionaryKey = keyof typeof dictionary;
+export type AncestryDictionary = { [k in AncestryDictionaryKey]: string };
 
 export default dictionary;
--- apps/consent-ui/src/i18n/locales/en/genetics-clinic.ts
+++ apps/consent-ui/src/i18n/locales/en/genetics-clinic.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 const dictionary = {
 	CHILDRENS_HOSPITAL_OF_EASTERN_ONTARIO_OTTAWA: 'Children’s Hospital of Eastern Ontario (Ottawa)',
 	GRAND_RIVER_HOSPITAL_KITCHENER: 'Grand River Hospital (Kitchener)',
@@ -20,7 +39,10 @@ const dictionary = {
 		'Thunder Bay Regional Health Sciences Centre (Thunder Bay)',
 	TRILLIUM_HEALTH_PARTNERS_MISSISSAUGA: 'Trillium Health Partners (Mississauga)',
 	WINDSOR_REGIONAL_HOSPITAL_WINDSOR: 'Windsor Regional Hospital (Windsor)',
-	WOMENS_COLLEGE_HOSPITAL_TORONTO: 'Women’s College Hospital (Toronto)',
-};
+	WOMENS_COLLEGE_HOSPITAL_TORONTO: "Women's College Hospital (Toronto)",
+} as const;
+
+export type GeneticsClinicDictionaryKey = keyof typeof dictionary;
+export type GeneticsClinicDictionary = { [k in GeneticsClinicDictionaryKey]: string };
 
 export default dictionary;
