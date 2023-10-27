5b70684436d70226459f5f69bb588e2f7fb55301
Sam Rich <samrichca@users.noreply.github.com>
2023-10-27 11:18:50 -0400

    revert seed data ID changes

--- apps/keys-das/prisma/seed/seed-data.ts
+++ apps/keys-das/prisma/seed/seed-data.ts
@@ -2,31 +2,31 @@ import { Prisma } from '../../src/generated/client/index.js';
 
 const ohipKeys: Prisma.OhipKeyCreateInput[] = [
 	{
-		participantId: 'buqF3qkmrXcuCZaZfXKmX',
+		participantId: 'cllgostgz000008l3fk0w',
 		ohipPrivateKey: 'Pli04iPLl7UwGHVROUesK',
 	},
 	{
-		participantId: '5PLJAg6LPDHKCzZUVamcg',
+		participantId: 'cllgoufw3000208l3c6gy',
 		ohipPrivateKey: 'Pli04iPLl7UwGHVROUrtd',
 	},
 	{
-		participantId: '9Cjl0sUDKnyW2TvQdAta6',
+		participantId: 'cllgouzph000308l35o99',
 		ohipPrivateKey: 'Pli04iPLl7UwGHVROU2se',
 	},
 ];
 
 const clinicalProfileKeys: Prisma.ClinicalProfileKeyCreateInput[] = [
 	{
-		participantId: 'NylQ1O6cMTFJDAbWHSnjm',
-		clinicalProfilePrivateKey: 'AFLH1BdZv4gI3As5h8r4T',
+		participantId: 'cllgostgz000008l3fk0w',
+		clinicalProfilePrivateKey: 'Cli04iPLl7UwGHVROU2se',
 	},
 	{
-		participantId: 'OiUvq8rNh1BY1H9DOSt7Y',
-		clinicalProfilePrivateKey: '21zkiwRtDwB2diY4cRH8M',
+		participantId: 'cllgoufw3000208l3c6gy',
+		clinicalProfilePrivateKey: 'Cli04iPLl7UwGHVROUewq',
 	},
 	{
-		participantId: 'oUrel7bpWvMhMWzmMRboX',
-		clinicalProfilePrivateKey: '2AwZtRhObB67crgoBkO3H',
+		participantId: 'cllgouzph000308l35o99',
+		clinicalProfilePrivateKey: 'Cli04iPLl7UwGHVROU4ds',
 	},
 ];
 
