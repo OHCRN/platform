63143290855908665f7b92ceada92c4d18a29459
Jon Eubank <joneubank@gmail.com>
2023-11-18 21:49:02 -0500

    ✨ (consent-api): Add error-handler to consent-api Express app

--- apps/consent-api/src/index.ts
+++ apps/consent-api/src/index.ts
@@ -22,16 +22,16 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import { AppConfig } from './config.js';
-import SwaggerRouter from './routers/swagger.js';
-import UserRouter from './routers/user.js';
-import StatusRouter from './routers/status.js';
-import ParticipantRouter from './routers/participants.js';
 import ConsentQuestionRouter from './routers/consentQuestions.js';
-import ParticipantResponseRouter from './routers/participantResponses.js';
 import ConsentCompletionRouter from './routers/consentCompletion.js';
 import ClinicianInviteRouter from './routers/invites.js';
-import RecaptchaRouter from './routers/recaptcha.js';
+import ParticipantResponseRouter from './routers/participantResponses.js';
+import ParticipantRouter from './routers/participants.js';
+import StatusRouter from './routers/status.js';
+import SwaggerRouter from './routers/swagger.js';
+import UserRouter from './routers/user.js';
 import WizardRouter from './routers/wizard.js';
+import errorHandler from 'error-handler';
 
 const App = (config: AppConfig) => {
 	const app = express();
@@ -50,15 +50,17 @@ const App = (config: AppConfig) => {
 
 	// set up routers
 	app.use('/api-docs', SwaggerRouter);
-	app.use('/user', UserRouter);
-	app.use('/status', StatusRouter);
-	app.use('/participants', ParticipantRouter);
-	app.use('/consent-questions', ConsentQuestionRouter);
-	app.use('/participant-responses', ParticipantResponseRouter);
+
 	app.use('/consent-completion', ConsentCompletionRouter);
+	app.use('/consent-questions', ConsentQuestionRouter);
 	app.use('/invites', ClinicianInviteRouter);
-	app.use('/recaptcha', RecaptchaRouter);
+	app.use('/participant-responses', ParticipantResponseRouter);
+	app.use('/participants', ParticipantRouter);
+	app.use('/status', StatusRouter);
+	app.use('/user', UserRouter);
 	app.use('/wizard', WizardRouter);
+
+	app.use(errorHandler);
 
 	return app;
 };
