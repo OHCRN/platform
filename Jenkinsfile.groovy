0ed0f089754bede491a332f3e5c15b35b6ce482d
Jon Eubank <joneubank@gmail.com>
2023-11-20 10:08:19 -0500

    💚 (root): Run generate script before build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -83,6 +83,7 @@ pipeline {
             steps {
                 container('node') {
                     sh 'npx --yes pnpm install'
+                    sh 'npx --yes pnpm generate'
                     sh 'npx --yes pnpm build'
                 }
             }
