5b0da2fd3a7a83a16bbd0f623808b97655561231
Alyssa D'Souza <70766739+alyssadsouza@users.noreply.github.com>
2023-12-07 13:48:40 -0500

    🦺 (pi-das): Add error handling to `GET /clinician-invites` route

--- apps/pi-das/src/services/search.ts
+++ apps/pi-das/src/services/search.ts
@@ -1,4 +1,10 @@
+import { Result, failure, success } from 'types/httpResponses';
+
 import prisma, { Participant, ClinicianInvite } from '../prismaClient.js';
+import { PrismaClientKnownRequestError } from '../generated/client/runtime/library.js';
+import serviceLogger from '../logger.js';
+
+const logger = serviceLogger.forModule('PrismaClient');
 
 export const getParticipant = async (participantId: string): Promise<Participant> => {
 	// TODO: add error handling
@@ -16,14 +22,34 @@ export const getParticipants = async (): Promise<Participant[]> => {
 	return result;
 };
 
-export const getClinicianInvite = async (inviteId: string): Promise<ClinicianInvite> => {
-	// TODO: add error handling
-	const result = await prisma.clinicianInvite.findUniqueOrThrow({
-		where: {
-			id: inviteId,
-		},
-	});
-	return result;
+type GetInviteFailureStatus = 'SYSTEM_ERROR' | 'INVITE_DOES_NOT_EXIST';
+
+export const getClinicianInvite = async (
+	inviteId: string,
+): Promise<Result<ClinicianInvite, GetInviteFailureStatus>> => {
+	return prisma.clinicianInvite
+		.findUniqueOrThrow({
+			where: {
+				id: inviteId,
+			},
+		})
+		.then((data) => success(data))
+		.catch((error) => {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2025') {
+					const errorMessage = `Invite with id '${inviteId}' does not exist.`;
+					logger.error('GET /invites', errorMessage, error.message);
+					return failure('INVITE_DOES_NOT_EXIST', errorMessage);
+				}
+				logger.error('GET /invites', error.code, error.message);
+				return failure(
+					'SYSTEM_ERROR',
+					`An unexpected error occurred in the PrismaClient - ${error.code}`,
+				);
+			}
+			logger.error('GET /invites', 'Unexpected error handling get invite request.', error.message);
+			return failure('SYSTEM_ERROR', 'An unexpected error occurred.');
+		});
 };
 
 export const getClinicianInvites = async (): Promise<ClinicianInvite[]> => {
