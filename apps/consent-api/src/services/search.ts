c7b868c0f924eb42b2f9b821510018f34d72940f
Alyssa D'Souza <70766739+alyssadsouza@users.noreply.github.com>
2023-12-20 13:28:34 -0500

    ✨ #170: Consent API GET route for Informed Consent responses (#318)

--- apps/consent-api/src/services/search.ts
+++ apps/consent-api/src/services/search.ts
@@ -1,7 +1,7 @@
 import urlJoin from 'url-join';
 import { AxiosError } from 'axios';
+import { ClinicianInviteResponse, InformedConsentResponse } from 'types/entities';
 import { Result, failure, success } from 'types/httpResponses';
-import { ClinicianInviteResponse } from 'types/entities';
 
 import serviceLogger from '../logger.js';
 import { getAppConfig } from '../config.js';
@@ -41,6 +41,43 @@ export const getLatestParticipantResponseByParticipantIdAndQuestionId = async (
 ): Promise<any> => {
 	// TODO: fetch from data-mapper, then return the result
 	return { participantId, consentQuestionId };
+};
+
+export type GetResponsesFailureStatus = 'SYSTEM_ERROR';
+
+/**
+ * Makes request to Data Mapper to get most recent responses for each INFORMED_CONSENT question
+ * @param participantId ID of participant to retrieve responses for
+ * @returns {InformedConsentResponse} Informed Consent questions and responses
+ */
+export const getInformedConsentResponses = async (
+	participantId: string,
+): Promise<Result<InformedConsentResponse, GetResponsesFailureStatus>> => {
+	try {
+		const { dataMapperUrl } = getAppConfig();
+		const { data } = await axiosClient.get(
+			urlJoin(dataMapperUrl, 'wizard', 'steps', 'informed-consent', participantId),
+		);
+		const participantResponses = InformedConsentResponse.safeParse(data);
+
+		if (!participantResponses.success) {
+			logger.error(
+				'Received invalid data in Informed Consent response',
+				participantResponses.error.issues,
+			);
+			return failure('SYSTEM_ERROR', participantResponses.error.message);
+		}
+
+		return success(participantResponses.data);
+	} catch (error) {
+		if (error instanceof AxiosError && error.response) {
+			const { data } = error.response;
+			logger.error('AxiosError handling Informed Consent request', data);
+			return failure('SYSTEM_ERROR', data.message);
+		}
+		logger.error('Unexpected error handling Informed Consent request.', error);
+		return failure('SYSTEM_ERROR', 'An unexpected error occurred.');
+	}
 };
 
 export type GetInviteFailureStatus = 'SYSTEM_ERROR' | 'INVITE_DOES_NOT_EXIST';
