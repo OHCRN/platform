ac99415443f1f48ad09ec1c550c507e8955b32e3
Andrew <xusandrew@gmail.com>
2024-02-09 11:18:30 -0500

    💄#271: Refactor translation in ConsentResearchParticipation component

--- apps/consent-ui/src/components/views/ConsentWizard/ConsentResearchParticipation/index.tsx
+++ apps/consent-ui/src/components/views/ConsentWizard/ConsentResearchParticipation/index.tsx
@@ -22,52 +22,15 @@ import clsx from 'clsx';
 import Link from 'next/link';
 
 import { ValidLanguage, getTranslation } from 'src/i18n';
-import { FormErrorsDictionary } from 'src/i18n/locales/en/formErrors';
-import { ConsentResearchParticipationPageDictionary } from 'src/i18n/locales/en/consentResearchParticipationPage';
-import { ConsentResearchParticipationFormDictionary } from 'src/i18n/locales/en/consentResearchParticipationForm';
 
 import styles from './ConsentResearchParticipation.module.scss';
 import ConsentResearchParticipationForm from './ConsentResearchParticipationForm';
 
 const ConsentResearchParticipation = ({ currentLang }: { currentLang: ValidLanguage }) => {
-	const translate = getTranslation(currentLang);
-
-	const formDict: ConsentResearchParticipationFormDictionary = {
-		researchParticipationFutureResearchTitle: translate(
-			'consentResearchParticipationForm',
-			'researchParticipationFutureResearchTitle',
-		),
-		researchParticipationFutureResearchTitleDesc: translate(
-			'consentResearchParticipationForm',
-			'researchParticipationFutureResearchTitleDesc',
-		),
-		researchParticipationContactInformationTitle: translate(
-			'consentResearchParticipationForm',
-			'researchParticipationContactInformationTitle',
-		),
-		researchParticipationContactInformationDesc: translate(
-			'consentResearchParticipationForm',
-			'researchParticipationContactInformationDesc',
-		),
-		researchParticipationContactInformationDescLink: translate(
-			'consentResearchParticipationForm',
-			'researchParticipationContactInformationDescLink',
-		),
-		yes: translate('consentResearchParticipationForm', 'yes'),
-		no: translate('consentResearchParticipationForm', 'no'),
-	};
-
-	const errorsDict: FormErrorsDictionary = {
-		required: translate('formErrors', 'required'),
-	};
-
-	const pageDict: ConsentResearchParticipationPageDictionary = {
-		heading: translate('consentResearchParticipationPage', 'heading'),
-		subheading: translate('consentResearchParticipationPage', 'subheading'),
-		subheadingLink: translate('consentResearchParticipationPage', 'subheadingLink'),
-		studyConsentPdf: translate('consentResearchParticipationPage', 'studyConsentPdf'),
-		smallText: translate('consentResearchParticipationPage', 'smallText'),
-	};
+	const { translateNamespace } = getTranslation(currentLang);
+	const formDict = translateNamespace('consentResearchParticipationForm');
+	const errorsDict = translateNamespace('formErrors');
+	const pageDict = translateNamespace('consentResearchParticipationPage');
 
 	const studyConsentPdfUrl = `/assets/pdfs/study-consent/${pageDict.studyConsentPdf}`;
 
