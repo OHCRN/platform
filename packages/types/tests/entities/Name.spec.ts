9dbf753555aa9ff62954f8accbf2063786bf236c
Sam Rich <samrichca@users.noreply.github.com>
2024-02-20 14:37:24 -0500

    update name tests

--- packages/types/tests/entities/Name.spec.ts
+++ packages/types/tests/entities/Name.spec.ts
@@ -25,6 +25,49 @@ describe('Name', () => {
 	it('Can be a string containing only letters', () => {
 		expect(Name.safeParse('Simpson').success).true;
 	});
+
+	it('Can be a string containing letters and spaces', () => {
+		expect(Name.safeParse('Homer Simpson').success).true;
+		expect(Name.safeParse('homer simpson').success).true;
+	});
+
+	it('Cannot be undefined', () => {
+		expect(Name.safeParse(undefined).success).false;
+	});
+
+	it('Cannot be an empty string', () => {
+		expect(Name.safeParse('').success).false;
+	});
+
+	it('Cannot be a string containing only whitespace', () => {
+		expect(Name.safeParse(' ').success).false;
+	});
+
+	it('Can be a string containing letters and whitespace', () => {
+		expect(Name.safeParse('homer      simpson').success).true;
+	});
+
+	it('Cannot be a string containing letters and numbers', () => {
+		expect(Name.safeParse('HomerSimpson1').success).false;
+	});
+
+	it('Cannot be a string containing letters, numbers and spaces', () => {
+		expect(Name.safeParse('Homer Simpson 1').success).false;
+	});
+
+	it('Cannot be a string containing only non-alphanumeric characters', () => {
+		expect(Name.safeParse('-_-').success).false;
+		expect(Name.safeParse("''").success).false;
+		expect(Name.safeParse('!!').success).false;
+		expect(Name.safeParse('*?').success).false;
+	});
+
+	it('Cannot be a string containing letters or numbers and non-alphanumeric characters', () => {
+		expect(Name.safeParse('-Homer Simpson').success).false;
+		expect(Name.safeParse("D'oh").success).false;
+		expect(Name.safeParse('Homer_Simpson3').success).false;
+		expect(Name.safeParse('Homer Simpon!').success).false;
+	});
 	// add necessary tests
 });
 
@@ -32,7 +75,49 @@ describe('OptionalName', () => {
 	it('Can be a string containing only letters', () => {
 		expect(OptionalName.safeParse('Simpson').success).true;
 	});
-	// add necessary tests
+
+	it('Can be a string containing letters and spaces', () => {
+		expect(OptionalName.safeParse('Homer Simpson').success).true;
+		expect(OptionalName.safeParse('homer simpson').success).true;
+	});
+
+	it('Can be undefined', () => {
+		expect(OptionalName.safeParse(undefined).success).true;
+	});
+
+	it('Cannot be an empty string', () => {
+		expect(OptionalName.safeParse('').success).false;
+	});
+
+	it('Cannot be a string containing only whitespace', () => {
+		expect(OptionalName.safeParse(' ').success).false;
+	});
+
+	it('Can be a string containing letters and whitespace', () => {
+		expect(OptionalName.safeParse('homer      simpson').success).true;
+	});
+
+	it('Cannot be a string containing letters and numbers', () => {
+		expect(OptionalName.safeParse('HomerSimpson1').success).false;
+	});
+
+	it('Cannot be a string containing letters, numbers and spaces', () => {
+		expect(OptionalName.safeParse('Homer Simpson 1').success).false;
+	});
+
+	it('Cannot be a string containing only non-alphanumeric characters', () => {
+		expect(OptionalName.safeParse('-_-').success).false;
+		expect(OptionalName.safeParse("''").success).false;
+		expect(OptionalName.safeParse('!!').success).false;
+		expect(OptionalName.safeParse('*?').success).false;
+	});
+
+	it('Cannot be a string containing letters or numbers and non-alphanumeric characters', () => {
+		expect(OptionalName.safeParse('-Homer Simpson').success).false;
+		expect(OptionalName.safeParse("D'oh").success).false;
+		expect(OptionalName.safeParse('Homer_Simpson3').success).false;
+		expect(OptionalName.safeParse('Homer Simpon!').success).false;
+	});
 });
 
 // TODO: add tests for special characters (accented letters) when added to the NAME_REGEX
