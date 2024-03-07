ee46ad7c908c27189bcb7598d0672608eebc8372
Andrew <xusandrew@gmail.com>
2024-03-07 11:47:02 -0500

    Add react-pdf dependency and PdfViewer component

---
+++ apps/consent-ui/src/components/common/PdfViewer/index.tsx
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+'use client';
+
+import { useState } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import { PDFDocumentProxy } from 'pdfjs-dist';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+// PDF worker
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+	'pdfjs-dist/build/pdf.worker.min.js',
+	import.meta.url,
+).toString();
+
+type PdfViewerProps = {
+	pdfUrl: string;
+};
+
+const PdfViewer = ({ pdfUrl }: PdfViewerProps) => {
+	const [numPages, setNumPages] = useState(0);
+
+	function onDocumentLoadSuccess({ numPages }: PDFDocumentProxy) {
+		setNumPages(numPages);
+	}
+
+	return (
+		<div>
+			<Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+				{Array.from(new Array(numPages), (el, index) => (
+					<Page key={`page_${index + 1}`} pageNumber={index + 1} />
+				))}
+			</Document>
+		</div>
+	);
+};
+
+export default PdfViewer;
